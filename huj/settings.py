ad248cb8819f6889ddd2252927a3a686257afc0d
bitesaitzzz <118013501+AliaksandrShydlouski1@users.noreply.github.com>
2023-09-04 14:32:05 +0300

    Update settings.py

--- huj/settings.py
+++ huj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y0chb&vf-gunav%jexigy&7(%i(u^8u)^$*uq)34ov5jqh#+&6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
