45135c459cc09ea6456bd50f9c1099724867262a
bitesaitzzz <118013501+AliaksandrShydlouski1@users.noreply.github.com>
2023-09-04 14:38:36 +0300

    Update settings.py

--- huj/settings.py
+++ huj/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'huj/main/static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'main/static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     "/var/www/static/",
