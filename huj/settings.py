ea89656e7dafdb60cedd0b982b99e17f113c2215
bitesaitzzz <118013501+AliaksandrShydlouski1@users.noreply.github.com>
2023-09-04 17:11:04 +0300

    Update settings.py

--- huj/settings.py
+++ huj/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), '/main/static')
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'huj/media')
 MEDIA_URL = '/media/'
