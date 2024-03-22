11b5bbd7495d2f95d0d7c5d5c5dacde0d26dcf70
bitesaitzzz <shidloalex@gmail.com>
2024-03-22 21:21:52 +0100

    change name folders

--- qc/asgi.py
+++ qc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for huj project.
+ASGI config for qc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qc.settings')
 
 application = get_asgi_application()
